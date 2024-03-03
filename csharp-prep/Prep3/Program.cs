4106fbf2e2eeebea7411de75cb73df6f76f18717
Maria Prieto <mariaprieto@byui.edu>
2024-03-02 21:59:59 -0300

    Finished Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Core 01
+        Random randomGenerator = new Random();
+        //int number = randomGenerator.Next(1, 11);
+
+        Console.Write("What is your guess? : ");
+        string numberUse = Console.ReadLine();
+        int numberGuess = int.Parse(numberUse);
+
+        //if (number > numberGuess)
+        //{Console.Write($"What is the magic number?: {number}");
+        //Console.Write(" Higher");}
+        //else if (number < numberGuess)
+        //{Console.Write($"What is the magic number?: {number}");
+        //Console.Write(" Lower");}
+        //else
+        //{Console.Write($"What is the magic number?: {number}");
+        //Console.Write(" You guessed it!");}
+
+        //Core 02
+
+
+        int numberCore = randomGenerator.Next(1, 100);
+
+
+        while (numberCore != numberGuess)
+            {
+
+                if (numberCore > numberGuess)
+            {
+                Console.WriteLine("Higher");
+                Console.Write("What is your guess? ");
+                numberGuess = int.Parse(Console.ReadLine());
+            }
+            else if (numberCore < numberGuess)
+            {
+                Console.WriteLine("Lower");
+                 Console.Write("What is your guess? ");
+                numberGuess = int.Parse(Console.ReadLine());
+            }
+            else 
+            {
+                Console.WriteLine("You guessed it!");
     }
 }
+}
+}
+
