32b1eada38661d27bc417b2c5f12e995ec891a86
Maria Prieto <mariaprieto@byui.edu>
2024-03-02 21:30:28 -0300

    Finished Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+          Console.Write("What is your grade?");
+        string grade = Console.ReadLine();
+        string gradeScore;
+
+        int x = int.Parse(grade);
+
+        if (x >= 90)
+        {
+           gradeScore = "A";
+            Console.WriteLine($"Your grade score is: {gradeScore}");
+        }
+        else if (x >=80)
+        {
+            gradeScore = "B";
+            Console.WriteLine($"Your grade score is: {gradeScore}");
+        }
+        else if (x >= 70)
+        {
+            gradeScore = "C";
+            Console.WriteLine($"Your grade score is: {gradeScore}");
+        }
+        else if (x >= 60)
+        {
+            gradeScore = "D";
+            Console.WriteLine($"Your grade score is: {gradeScore}");
+        }
+        else
+        {
+            gradeScore = "F";
+            Console.WriteLine($"Your grade score is: {gradeScore}");
+        }
+        if (gradeScore =="F")
+        {
+            Console.WriteLine("Your grade is not enough to pass this course, continue studying");
+        }
+        else
+        {
+            Console.WriteLine("Congratulations, you have passed this course");
+        }
     }
 }
